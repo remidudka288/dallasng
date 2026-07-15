0c19d83a086b2ffc7a79566f5c13cfc307522f62
Remi Dudka <remigiusz.dudka@outlook.com>
2026-07-15 15:22:46 +0200

    init

---
+++ components/dallasng/__init__.py
@@ -0,0 +1,31 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.const import CONF_ID, CONF_PIN
+
+MULTI_CONF = True
+AUTO_LOAD = ["sensor"]
+
+dallasng_ns = cg.esphome_ns.namespace("dallasng")
+DallasNgComponent = dallasng_ns.class_("DallasNgComponent", cg.PollingComponent)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(DallasNgComponent),
+        cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
+    }
+).extend(cv.polling_component_schema("60s"))
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    cg.add_build_flag("-DOWNG_NO_NATIVE_CONFIG")
+    cg.add_library(
+        "OneWireNg",
+        None,
+        "https://github.com/pstolarz/OneWireNg.git#0.12.3",
+    )
+
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
